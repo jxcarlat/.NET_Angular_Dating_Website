fd608d0a03f17ed216b57f05aee5476b3b07bbed
jxcarlat <joshbusiness22@gmail.com>
2023-03-16 14:04:16 -0500

    End of section 12

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,11 +1,20 @@
 using System.ComponentModel.DataAnnotations;
-
+using System.Text.Json.Serialization;
+using API.Converter;
 namespace API.DTOs
 {
     public class RegisterDto
     {
         [Required]
         public string Username { get; set; }
+
+        [Required] public string KnownAs { get; set; }
+        [Required] public string Gender { get; set; }
+        [JsonConverter(typeof(DateOnlyJsonConverter))]
+        [Required] public DateOnly? DateOfBirth { get; set; } // optional to make required work!
+        [Required] public string City { get; set; }
+        [Required] public string Country { get; set; }
+
         [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
